3011c975d67ecd97c21875997d09d529a40502f6
Ryan Bigg <me@ryanbigg.com>
2021-07-21 10:43:45 +1000

    Add work exclusions to work page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     enabled: true,
-    content: ["./*.html"]
+    content: ["./*.html", "./_includes/**/*.html"]
   },
   darkMode: 'media', // or 'media' or 'class'
   theme: {
